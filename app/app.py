0e7472af16e94b5a22d95be80451078f0726b36a
shahpranshu27 <shahpranshu2712@gmail.com>
2026-01-08 18:39:42 +0530

    query param, request body & POST

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate
 
 app = FastAPI()
 
@@ -16,9 +17,12 @@ text_posts = {
     11: {"title": "summary post", "content": "short summary content"},
 }
 
-
+# limit: int = None -> optional query parameter
+# limit: int -> mandatory query parameter
 @app.get("/posts")
-def get_all_posts():
+def get_all_posts(limit: int = None):
+    if limit:
+        return list(text_posts.values())[:limit]
     return text_posts
 
 
@@ -28,3 +32,10 @@ def get_post(id: int):
     if id not in text_posts:
         raise HTTPException(status_code=404, detail="Post not found!")
     return text_posts.get(id)
+
+
+@app.post("/posts")
+def create_post(post: PostCreate): # for validation
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
